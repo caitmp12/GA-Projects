78444062b3e9c8c6e0708f42907cf160a7989716
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-08-27 21:04:59 -0400

    project updates

--- p1/Marvel_Memory/app2.js
+++ p1/Marvel_Memory/app2.js
@@ -1,14 +1,18 @@
 
-const characterNums = [346, 659, 332, 149, 107, 313]
 
-const idNum = () => {
-    for (let i=0; i<characterNums.length; i++){
-        
-    }
-}
+
+// const characterNums = [346, 659, 332, 149, 107, 313]
+
+// const idNum = () => {
+//     for (let i = 0; i < characterNums.length; i++){  
+//       return characterNums[i]  
+//     }
+// }
+
+// idNum()
 
 $.ajax({
-    url: 'https://www.superheroapi.com/api.php/10218014924347774/346'
+    url: `https://www.superheroapi.com/api.php/10218014924347774/346`
 }).then(
     (data) => {
         console.log(data)
@@ -20,3 +24,21 @@ $.ajax({
         console.log("bad")
     }
 )
+
+
+
+////Variables
+
+// const memoryArr = ["Iron Man", "Thor", "The Hulk", "Captain America", "Black Widow", "Hawkeye"];
+
+// const memoryValues = [];
+
+// const cardsFlipped = 0;
+
+
+////Game Logic
+
+$("#loki-pic").on("click", () => {
+    prompt
+    // $(".grid-container").empty()
+})
